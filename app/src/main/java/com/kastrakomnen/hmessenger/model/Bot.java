01f373d21189a7c6b4dd78b41527abd0442c8dee
hsynydn <ce.hsynydn@gmail.com>
2022-06-12 19:20:23 +0300

    feat: Add bot

---
+++ app/src/main/java/com/kastrakomnen/hmessenger/model/Bot.java
@@ -0,0 +1,65 @@
+package com.kastrakomnen.hmessenger.model;
+
+import android.util.Log;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class Bot implements GameStateListener{
+
+    private GameInputListener gameInputListener;
+    private BotBehaviour botBehaviour;
+
+    private final ScheduledExecutorService service;
+
+    public Bot(BotBehaviour botBehaviour){
+        this.botBehaviour = botBehaviour;
+        service = Executors.newScheduledThreadPool(1);
+    }
+
+    public void registerGameInputListener(GameInputListener gameInputListener){
+        this.gameInputListener = gameInputListener;
+    }
+
+    @Override
+    public void onGameStart() {
+        service.scheduleAtFixedRate(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        gameInputListener.onMoveDown();
+                    }
+                },
+                botBehaviour.botStartDelay,
+                botBehaviour.commandGenerationPeriod,
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    @Override
+    public void onGamePause() {
+        service.shutdown();
+    }
+
+    @Override
+    public void onGameResume() {
+        service.scheduleAtFixedRate(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        gameInputListener.onMoveDown();
+                    }
+                },
+                botBehaviour.botStartDelay,
+                botBehaviour.commandGenerationPeriod,
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    @Override
+    public void onGameStop() {
+        service.shutdown();
+    }
+}
---
+++ app/src/main/java/com/kastrakomnen/hmessenger/model/BotBehaviour.java
@@ -0,0 +1,9 @@
+package com.kastrakomnen.hmessenger.model;
+
+public class BotBehaviour {
+    /* Number of milliseconds on which new command will be generated */
+    public int commandGenerationPeriod;
+
+    /* Number of milliseconds to delay bot start-up */
+    public int botStartDelay;
+}

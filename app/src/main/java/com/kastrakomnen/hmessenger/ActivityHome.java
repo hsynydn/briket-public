a648624085c232afb15f41d5100c98785b6609b0
hsynydn <ce.hsynydn@gmail.com>
2022-07-31 11:19:28 +0300

    style: Add progress in home activity

--- app/src/main/java/com/kastrakomnen/hmessenger/ActivityHome.java
+++ app/src/main/java/com/kastrakomnen/hmessenger/ActivityHome.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -22,6 +25,7 @@ import com.google.android.gms.games.GamesSignInClient;
 import com.google.android.gms.games.PlayGames;
 import com.google.android.gms.games.PlayGamesSdk;
 import com.kastrakomnen.hmessenger.model.BriketContext;
+import com.kastrakomnen.hmessenger.view.ProgressCard;
 
 public class ActivityHome extends AppCompatActivity {
 
@@ -32,6 +36,13 @@ public class ActivityHome extends AppCompatActivity {
     private Animation animaton_3;
     private Animation logoBreathingAnimation;
     private Animation playFrameHorizontalBreathingAnimation;
+
+    private ImageView drop_2;
+    private ImageView drop_3;
+
+    private ProgressBar progressBar;
+    private TextView loading;
+    private ImageView layout_home_iv_play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +61,11 @@ public class ActivityHome extends AppCompatActivity {
             Log.i(TAG, e.toString());
         }
 
-        animaton = AnimationUtils.loadAnimation(this, R.anim.dropping_bricks);
-        ImageView drop_1 = findViewById(R.id.drop_1);
-        drop_1.setAnimation(animaton);
-
         animaton_2 = AnimationUtils.loadAnimation(this, R.anim.dropping_bricks_2);
-        ImageView drop_2 = findViewById(R.id.drop_2);
-        drop_2.setAnimation(animaton_2);
+        drop_2 = findViewById(R.id.drop_2);
 
         animaton_3 = AnimationUtils.loadAnimation(this, R.anim.dropping_bricks_3);
-        ImageView drop_3 = findViewById(R.id.iv_home_drop_3);
-        drop_3.setAnimation(animaton_3);
+        drop_3 = findViewById(R.id.iv_home_drop_3);
 
         this.logoBreathingAnimation = AnimationUtils.loadAnimation(this, R.anim.breathing);
         findViewById(R.id.iv_logo).setAnimation(logoBreathingAnimation);
@@ -68,21 +73,34 @@ public class ActivityHome extends AppCompatActivity {
         this.playFrameHorizontalBreathingAnimation = AnimationUtils.loadAnimation(this, R.anim.horizontal_breathing);
         findViewById(R.id.layout_home_iv_play).setAnimation(playFrameHorizontalBreathingAnimation);
 
-        findViewById(R.id.home_pause_btn_exit).setOnClickListener(view -> {
-            Log.i(TAG, "Exit");
-            this.finishAffinity();
-        });
+//        findViewById(R.id.home_pause_btn_exit).setOnClickListener(view -> {
+//            Log.i(TAG, "Exit");
+//            this.finishAffinity();
+//        });
 
-        findViewById(R.id.layout_home_iv_play).setOnTouchListener(new View.OnTouchListener() {
+        layout_home_iv_play = findViewById(R.id.layout_home_iv_play);
+        layout_home_iv_play.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.i(TAG, "Play");
+                Log.i(TAG, "Play button pressed");
                 Intent intent = new Intent(ActivityHome.this, ActivityGameMap.class);
                 startActivity(intent);
                 ActivityHome.this.finish();
                 return true;
             }
         });
+
+        layout_home_iv_play.setVisibility(View.GONE);
+        progressBar = findViewById(R.id.progressBar);
+        loading = findViewById(R.id.lout_home_textview_loading);
+
+        drop_2.setAnimation(animaton_2);
+        drop_3.setAnimation(animaton_3);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         AsyncTask.execute(new Runnable() {
             @Override
@@ -92,13 +110,19 @@ public class ActivityHome extends AppCompatActivity {
                 BriketContext.getInstance().initializeAdMob(ActivityHome.this);
                 BriketContext.getInstance().initializeGooglePlayServices(ActivityHome.this);
                 Log.d(TAG, "BriketContext.loading finished");
+
+                SystemClock.sleep(2000);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        loading.setVisibility(View.INVISIBLE);
+                        layout_home_iv_play.setVisibility(View.VISIBLE);
+                    }
+                });
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
     }
 
     @Override
